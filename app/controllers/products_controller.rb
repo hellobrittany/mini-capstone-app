0cadf4d6803d1c4c56ec84627fec9746168da070
hellobrittany <hello@brittanywetzel.com>
2017-05-08 21:35:47 -0500

    added delete, flash, redirect

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,5 +20,32 @@ class ProductsController < ApplicationController
 												price: params[:price] 
 												)
 		product.save
+
+		flash[:success] = "Product Successfully Created"
+		redirect_to "/products/#{product.id}"
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+	product = Product.find(params[:id])
+		product.assign_attributes(
+															name: params[:name], 
+															image: params[:image], 
+															description: params[:description], 
+															price: params[:price])
+		product.save
+		flash[:success] = "Product successfully updated"
+		redirect_to "/products/#{product.id}"
+	end
+
+	def destroy
+		product = Product.find(params[:id])
+		product.destroy
+
+		flash[:warning] = "Product destroyed"
+		redirect_to "/"
 	end
 end
