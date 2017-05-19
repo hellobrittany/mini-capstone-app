f64463d8ce6121fb000e7e830b8abbe6ced0a59b
hellobrittany <hello@brittanywetzel.com>
2017-05-18 21:31:31 -0500

    added orders model and restful routing

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
+  post '/orders' => 'orders#create'
+  get '/orders/:id' => 'orders#show'
+
+
 
 
 
